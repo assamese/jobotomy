00b2e6d5e115ea13b37be3ddbb39c029ee3fd201
Sanjay Das <assamese@hotmail.com>
2017-01-02 12:54:33 -0500

    Add files via upload

---
+++ mdspost2nodebb/config.js
@@ -0,0 +1,17 @@
+
+
+/* aws-jobotomy */
+module.exports = {
+
+  "database":"mongodb://localhost:27017/techskills",
+  "nodebbhost":"http://52.26.101.153:4567/api/v1/topics?_uid=1",
+  "secretKey" : "xxx",
+  "startDate" : "2016-03-08T00:04:35.630Z",
+  "mastertoken" : "xxx",
+  "cidForJobs" : "7",
+  "cidForEmployers" : "8"
+
+};
+
+
+
